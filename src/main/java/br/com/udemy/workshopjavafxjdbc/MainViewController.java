c413ab99dc464c7af36ef971d219bacef32b3aea
marcusviniciusls <viniciusmls@outlook.com>
2022-03-10 21:13:58 -0300

    Main view controller

---
+++ src/main/java/br/com/udemy/workshopjavafxjdbc/MainViewController.java
@@ -0,0 +1,36 @@
+package br.com.udemy.workshopjavafxjdbc;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("Seller");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("Department");
+    }
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("About");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
